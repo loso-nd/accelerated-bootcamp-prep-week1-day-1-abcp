496716e5b58de3d3ba7f636e686276c2d9d9bb63
Nahomie ND <nducasse3@gmail.com>
2018-06-07 21:59:22 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -12,4 +12,4 @@ console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some porti
 // Now that we understand what a variable is
 // rewrite the above lines of code using a variable to store the
 // total sum, so that you don't have to add up all those numbers three times
-var totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
+totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
